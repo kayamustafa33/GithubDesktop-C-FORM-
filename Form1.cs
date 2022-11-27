b1f5c7c3cbe8079e562bd695ac25a841d72eb4ca
Mustafa <89656051+kayamustafa33@users.noreply.github.com>
2022-11-27 21:11:32 +0200

    Add files via upload
    First WebView App with C# Windows Form

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Github_Desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initBrowser();
+        }
+
+        private async Task initizated()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+        }
+
+        public async void initBrowser()
+        {
+            await initizated();
+            webView21.CoreWebView2.Navigate("https://github.com/");
+        }
+    }
+}
